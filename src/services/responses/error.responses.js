7af2249631148724da833d883f3e3ef59aa98c68
murilosntdev <murilosntdev@gmail.com>
2024-11-09 20:49:08 -0300

    error response mounter

---
+++ src/services/responses/error.responses.js
@@ -0,0 +1,28 @@
+export const errorResponse = (statusCode, details, debugInfo) => {
+    const response = {
+        error: {}
+    };
+
+    switch (statusCode) {
+        case 422: {
+            response.error.status = 422;
+            response.error.message = "Entidade Não Processável";
+            break;
+        };
+        default: {
+            response.error.status = 500;
+            response.error.message = "Erro do Servidor Interno";
+            break;
+        };
+    };
+
+    if (details) {
+        response.error.details = details;
+    };
+
+    if (process.env.SYSTEM_SHOW_DEBUG_INFO === "true" && debugInfo) {
+        response.error.debugInfo = debugInfo;
+    };
+
+    return (response);
+};
