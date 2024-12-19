befbb87f9f88e1d006c4a02252d6fb86830cb7e6
murilosntdev <murilosntdev@gmail.com>
2024-12-19 15:29:39 -0300

    bug: on disconnect delete all empty rooms

--- src/socket/handlers/connection.handlers.js
+++ src/socket/handlers/connection.handlers.js
@@ -2,12 +2,14 @@ import { activeRooms } from "./rooms.handlers.js";
 
 export function disconnectHandler(socket) {
     for (const [roomId, roomData] of Object.entries(activeRooms)) {
-        roomData.players = roomData.players.filter((playerUsername) => playerUsername !== socket.user);
+        if (roomData.players.includes(socket.user)) {
+            roomData.players = roomData.players.filter((playerUsername) => playerUsername !== socket.user);
 
-        socket.leave(roomId);
+            socket.leave(roomId);
 
-        if (roomData.players.length === 0) {
-            delete activeRooms[roomId];
+            if (roomData.players.length === 0) {
+                delete activeRooms[roomId];
+            };
         };
     };
 };
