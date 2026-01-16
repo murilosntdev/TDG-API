03723d5e705efabd95c67d836c5f0819a4dd5a6d
murilosntdev <murilosntdev@gmail.com>
2026-01-15 21:11:27 -0300

    fix: handlebars views path adjust 4

--- src/core/email/email.js
+++ src/core/email/email.js
@@ -3,9 +3,15 @@ import path from "path";
 import hbs from "nodemailer-express-handlebars";
 import { fileURLToPath } from "url";
 
+console.log("--- [EMAIL.JS LOG] INICIANDO O MÓDULO DE EMAIL ---");
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const viewsPath = path.join(__dirname, "views");
+
+console.log(`--- [EMAIL.JS LOG] __filename: ${__filename}`);
+console.log(`--- [EMAIL.JS LOG] __dirname: ${__dirname}`);
+console.log(`--- [EMAIL.JS LOG] viewsPath final: ${viewsPath}`);
 
 const smtp = nodemailer.createTransport({
     host: process.env.NODEMAILER_HOST,
@@ -31,6 +37,7 @@ const handleBarsOptions = {
 smtp.use("compile", hbs(handleBarsOptions));
 
 export const sendMail = (recipientEmail, subject, template, context) => {
+    console.log("--- [EMAIL.JS LOG] Função sendMail foi chamada. ---");
     const configEmail = {
         from: `Truco da Galera <${process.env.NODEMAILER_USER}>`,
         to: [recipientEmail],
@@ -44,6 +51,8 @@ export const sendMail = (recipientEmail, subject, template, context) => {
             response(res);
             smtp.close();
         }).catch(error => {
+            console.log("--- [EMAIL.JS LOG] ERRO DENTRO DO SENDMAIL ---");
+            console.error(error);
             const errorContent = {};
             errorContent.emailError = error;
             response(errorContent);
